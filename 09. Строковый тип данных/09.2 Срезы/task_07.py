405ff949f4981f830ad5e855cee51377e8ec4d38
Iskender Dzhakhangirli <ijahaqa@gmail.com>
2025-05-28 12:04:00 +0400

    20250528

---
+++ 09. Строковый тип данных/09.2 Срезы/task_07.py
@@ -0,0 +1,11 @@
+'''
+Используя срезы, дополните приведённый ниже код так, чтобы 
+он вывел первые 12 символов строки s.
+'''
+
+# example
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print()
+
+# solution
+print(s[:12])
