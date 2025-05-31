1b1f21fe225cf31ae68e53c496ef1ad5e097a876
Iskender Dzhakhangirli <ijahaqa@gmail.com>
2025-05-31 14:58:13 +0400

    20250531

---
+++ 11. Списки/11.2 Основы работы со списками/task_06.py
@@ -0,0 +1,14 @@
+'''
+Дополните приведённый ниже код так, 
+чтобы он вывел среднее арифметическое элементов списка evens.
+'''
+
+# example
+evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+average = ...
+print(average)
+
+# review
+evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+average = sum(evens) / len(evens)
+print(average)
---
+++ 11. Списки/11.2 Основы работы со списками/task_08.py
@@ -0,0 +1,13 @@
+'''
+Дополните приведённый ниже код так, 
+чтобы он вывел "перевёрнутый" список languages 
+(то есть элементы будут идти в обратном порядке).
+Для вывода списка воспользуйтесь функцией print().
+'''
+
+# example
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+
+# review
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+print(languages[::-1])
