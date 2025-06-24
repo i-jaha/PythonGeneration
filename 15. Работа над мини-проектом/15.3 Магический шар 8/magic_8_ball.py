3a3b4822953fb648a2e8adc516d0aafa6a3d2779
Iskender Dzhakhangirli <ijahaqa@gmail.com>
2025-06-24 10:51:15 +0400

    20250624

--- 15. Работа над мини-проектом/15.3 Магический шар 8/magic_8_ball.py
+++ 15. Работа над мини-проектом/15.3 Магический шар 8/magic_8_ball.py
@@ -1,79 +1,79 @@
 '''
-Магический шар 8
-Описание проекта: 
-магический шар 8 (шар судьбы) — шуточный способ предсказывать будущее. 
-Программа должна просить пользователя задать некий вопрос, 
-чтобы случайным образом на него ответить.
+Magic Ball 8
+Project Description:
+Magic Ball 8 (Fortune Ball) is a humorous way to predict the future.
+The program should ask the user to enter a certain question,
+and then randomly generate an answer.
 
-Составляющие проекта:
-    Целые числа (тип int);
-    Переменные;
-    Ввод / вывод данных (функции input() и print());
-    Условный оператор (if/elif/else);
-    Цикл while;
-    Бесконечный цикл;
-    Операторы break, continue;
-    Работа с модулем random для генерации случайных чисел.
+Project Components:
+    Integers (int type);
+    Variables;
+    Data input/output (input() and print() functions);
+    Conditional statements (if/elif/else);
+    While loop;
+    Infinite loop;
+    Break and continue statements;
+    Working with the random module to generate random numbers.
 
-Варианты ответов
-Традиционно шар имеет 20 ответов, которые можно разделить на четыре группы.
-| Положительные             | Нерешительно положительные | Нейтральные                    | Отрицательные                |
-| ------------------------- | -------------------------- | ------------------------------ | ---------------------------- |
-| Бесспорно                 | Мне кажется - да           | Пока неясно, попробуй снова    | Даже не думай                |
-| Предрешено                | Вероятнее всего            | Спроси позже                   | Мой ответ - нет              |
-| Никаких сомнений          | Хорошие перспективы        | Лучше не рассказывать          | По моим данным - нет         |
-| Определённо да            | Знаки говорят - да         | Сейчас нельзя предсказать      | Перспективы не очень хорошие |
-| Можешь быть уверен в этом | Да                         | Сконцентрируйся и спроси опять | Весьма сомнительно           |
+Answer Options
+Traditionally, the Magic 8 Ball has 20 answers, which can be divided into four groups.
+|Positive          |Tentatively Positive|Neutral                      |Negative                        |
+|------------------|--------------------|-----------------------------|--------------------------------|
+|It is certain     |I think so - yes    |Cannot predict now           |Don&#39;t even think about it   |
+|It is decidedly so|Probably            |Ask again later              |My reply is no                  |
+|Without a doubt   |Good prospects      |Better not tell you now      |According to my information - no|
+|Definitely yes    |Signs point to yes  |Cannot be predicted right now|Prospects are not very good     |
+|You may rely on it|Yes                 |Concentrate and ask again    |Very doubtful                   |
 
-Примечания
-Примечание 1. 
-Внутри магического шара есть ёмкость с тёмной жидкостью, например, чернилами. 
-В жидкости плавает фигура с 20 гранями, на каждой из них нанесено по одному ответу.
+Notes
+Note 1.
+Inside the magic ball there is a container with dark liquid, for example, ink.
+A 20-sided figure floats in the liquid, with one answer printed on each side.
 
-Примечание 2. 
-Многогранник с 20 гранями называется икосаэдр.
+Note 2.
+A polyhedron with 20 faces is called an icosahedron.
 
-Заголовок программы
-Подключите модуль random;
-Создайте список answers, содержащий 20 потенциальных ответов (Бесспорно, Предрешено, и т.д.).
+Program title
+Connect the random module;
+Create a list of answers containing 20 potential responses (Certainly, Predetermined, etc.)
 
-Приветствие пользователя
-Выведите текстовое сообщение: 
-'Привет Мир, я магический шар, и я знаю ответ на любой твой вопрос.';
-Уточните как зовут пользователя;
-Выведите слова приветствия, например, 'Привет Тимур'.
+User Greeting
+Display the text message:
+'Hello World, I am a magic ball, and I know the answer to any question you have.'
+Clarify the user's name;
+Display the greeting words, for example, 'Hello Timur'.
 
-Основной цикл программы
-Организуйте цикл, который будет запрашивать у пользователя данные;
-Запросите у пользователя вопрос;
-Выведите случайный ответ с помощью функции choice(), 
-передав список answers в качестве аргумента;
-Уточните у пользователя, хочет ли он задать еще один вопрос, 
-если да, то вернитесь в основной цикл программы, 
-если нет, выведите сообщение 
-'Возвращайся, если возникнут вопросы!' и завершите программу.
+Main program loop
+Set up a loop that will ask the user for input;
+Prompt the user to ask a question;
+Use the choice() function to display a random answer,
+passing the list "answers" as an argument;
+Ask the user if they want to ask another question,
+if yes, return to the main loop of the program,
+if no, display the message
+'Come back if you have any more questions!' and exit the program.
 '''
 
 import random
 
-print('Привет Мир, я магический шар, и я знаю ответ на любой твой вопрос.')
-user_name = input('Как тебя зовут? ')
-print(f'Привет {user_name}')
-print("Введите 'выход' для завершения программы.\n")
+print('Hello World, I am a magic ball, and I know the answer to any question you have.')
+user_name = input('What is your name? ')
+print(f'Hello {user_name}')
+print("Enter 'exit' to terminate the program.\n")
 
 answers = [
-    "Бесспорно", "Предрешено", "Никаких сомнений", "Определённо да", "Можешь быть уверен в этом",
-    "Мне кажется - да", "Вероятнее всего", "Хорошие перспективы", "Знаки говорят - да", "Да",
-    "Пока неясно, попробуй снова", "Спроси позже", "Лучше не рассказывать", "Сейчас нельзя предсказать", "Сконцентрируйся и спроси опять",
-    "Даже не думай", "Мой ответ - нет", "По моим данным - нет", "Перспективы не очень хорошие", "Весьма сомнительно"
+    "It is certain", "It is decidedly so", "Without a doubt", "Definitely yes", "You may rely on it",
+    "I think so - yes", "Probably", "Good prospects", "Signs point to yes", "Yes",
+    "Cannot predict now", "Ask again later", "Better not tell you now", "Cannot be predicted right now", "Concentrate and ask again",
+    "Don't even think about it", "My reply is no", "According to my information - no", "Prospects are not very good", "Very doubtful"
 ]
 
 while True:
-    question = input("Задайте ваш вопрос: ")
+    question = input("Ask your question: ")
     answer = random.choice(answers)
     print(answer)
     
-    continue_question = input("Хотите задать еще один вопрос? (да/нет): ")
-    if continue_question.lower() != "да":
-        print("Возвращайся, если возникнут вопросы!")
+    continue_question = input("Would you like to ask another question? (yes/no): ")
+    if continue_question.lower() != "yes":
+        print("Come back if you have any questions!")
         break
