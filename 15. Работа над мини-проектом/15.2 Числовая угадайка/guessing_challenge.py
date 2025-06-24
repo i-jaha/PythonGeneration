6e271d30e4bf1587649f29696d2097a3bf4975ea
Iskender Dzhakhangirli <ijahaqa@gmail.com>
2025-06-24 10:39:17 +0400

    20250624

--- 15. Работа над мини-проектом/15.2 Числовая угадайка/guessing_challenge.py
+++ 15. Работа над мини-проектом/15.2 Числовая угадайка/guessing_challenge.py
@@ -1,66 +1,65 @@
 """
-Угадайка чисел
-Описание проекта:
-программа генерирует случайное число в диапазоне от 1 до 100
-и просит пользователя угадать это число.
-Если догадка пользователя больше случайного числа,
-то программа должна вывести сообщение 'Слишком много, попробуйте еще раз'.
-Если догадка меньше случайного числа,
-то программа должна вывести сообщение 'Слишком мало, попробуйте еще раз'.
-Если пользователь угадывает число,
-то программа должна поздравить его и вывести сообщение 'Вы угадали, поздравляем!'.
+Number Guessing Game
+Project Description:
+The program generates a random number in the range from 1 to 100
+and asks the user to guess this number.
+If the user's guess is higher than the random number,
+the program should display the message 'Too high, try again'.
+If the guess is lower than the random number,
+the program should display the message 'Too low, try again'.
+If the user guesses the number correctly,
+the program should congratulate them and display the message 
+'You guessed it, congratulations!'.
 
-Составляющие проекта:
-    - Целые числа (тип int);
-    - Переменные;
-    - Ввод / вывод данных (функции input() и print());
-    - Условный оператор (if/elif/else);
-    - Цикл while;
-    - Бесконечный цикл;
-    - Операторы break, continue;
-    - Работа с модулем random для генерации случайных чисел.
+Project components:
+    - Integers (int type);
+    - Variables;
+    - Data input/output (input() and print() functions);
+    - Conditional operator (if/elif/else);
+    - While loop;
+    - Infinite loop;
+    - Break and continue operators;
 
-Заголовок программы
-Подключите модуль random;
-Сгенерируйте случайное число от 1 до 100;
-Выведите текст приветствия пользователю: 'Добро пожаловать в числовую угадайку'.
+Program Title
+Connect the random module;
+Generate a random number from 1 to 100;
+Display the welcome message to the user: 'Welcome to the Number Guessing Game'
 
-Функция проверки корректности введенных данных
-Пользователь потенциально может ввести неверные данные,
-например, не число, или число превышающее 100.
-Важно предусмотреть такую возможность, чтобы программа продолжала правильно работать.
-Обработка такого рода ситуаций называется защитой от дурака.
-Напишите функцию is_valid() в которую передается один строковый аргумент.
-Функция возвращает значение True,
-если переданный аргумент является целым числом от 1 до 100,
-и False в противном случае.
+Validation function for input data
+A user may potentially enter incorrect data,
+for example, a non-numeric value or a number exceeding 100.
+It's important to account for such cases to ensure the program continues to operate correctly.
+Handling such situations is referred to as "foolproofing" (or "idiot-proofing").
+Write a function called is_valid() that takes one string argument.
+The function returns True
+if the passed argument is an integer between 1 and 100 inclusive,
+and False otherwise.
 
-Основной цикл программы
-Организуйте цикл, который будет запрашивать у пользователя данные
-(цикл может быть бесконечным (while True)
-или может использовать сигнальную метку с последующим переключением, после угадывания числа);
-Запросите у пользователя число от 1 до 100;
-Проверьте введенные данные с помощью функции is_valid():
-если данные некорректны, выведите текст: 'А может быть все-таки введем целое число от 1 до 100?' и перейдите к следующей итерации основного цикла;
-если данные корректны, преобразуйте их к целому числу для удобства дальнейшей работы.
+Main program loop
+Organize a loop that will request data from the user
+(the loop can be infinite (while True)
+or it can use a flag variable which changes its state after the number is guessed);
+Prompt the user for a number between 1 and 100;
+Check the entered data using the is_valid() function:
+if the data is incorrect, display the message: 'Maybe we should enter an integer between 1 and 100?' and proceed to the next iteration of the main loop;
+if the data is correct, convert it to an integer for convenience in further processing.
 
-Сравнение введенного числа с загаданным
-Организуйте сравнение введенного числа с загаданным числом:
-Если введенное число меньше загаданного числа,
-выведите текст: 'Ваше число меньше загаданного, попробуйте еще разок';
-Если введенное число больше загаданного числа,
-выведите текст: 'Ваше число больше загаданного, попробуйте еще разок';
-Если введенное число равно загаданному числу,
-выведите текст: 'Вы угадали, поздравляем!'.
-Выведите прощальное сообщение пользователю:
-'Спасибо, что играли в числовую угадайку. Еще увидимся...'.
+Comparison of the entered number with the guessed number
+Implement the comparison between the entered number and the guessed number:
+If the entered number is less than the guessed number,
+display the message: 'Your number is smaller than the guessed one. Try again.'
+If the entered number is greater than the guessed number,
+display the message: 'Your number is greater than the guessed one. Try again.'
+If the entered number equals the guessed number,
+display the message: 'You guessed it, congratulations!'
+Display a farewell message to the user:
+'Thank you for playing the number guessing game. See you again...'
 
-Улучшения проекта
-Добавьте подсчет попыток, сделанных пользователем.
-Когда число отгадано, программа должна показать количество попыток;
-Добавьте возможность генерации нового числа (повторная игра), после того,
-как пользователь угадал число;
-Добавить возможность указания правой границы для случайного выбора числа (от 1 до n).
+Project Improvements
+Add a counter for the number of attempts made by the user.
+Once the number is guessed, the program should display the total number of attempts;
+Add the ability to generate a new number (start a new game) after the user has correctly guessed the number;
+Add the option to specify the upper limit for random number generation (from 1 to n).
 """
 
 import random
@@ -74,35 +73,35 @@ def is_valid(s, max_num):
         return False
 
 
-print("Добро пожаловать в числовую угадайку")
+print("Welcome to the Number Guessing Game")
 
 while True:
-    max_number = int(input("Введите правую границу для загадывания числа: "))
-    print(f"Я загадал число от 1 до {max_number}. Попробуйте его угадать!")
+    max_number = int(input("Enter the right boundary for guessing the number: "))
+    print(f"I made up a number from 1 to {max_number}. Try to guess it!")
 
     random_number = random.randint(1, max_number)
     attempts = 0
 
     while True:
-        user_input = input("Введите ваше число: ")
+        user_input = input("Enter your number: ")
 
         if not is_valid(user_input, max_number):
-            print(f"А может быть все-таки введем целое число от 1 до {max_number}?")
+            print(f"But maybe we should still enter an integer from 1 to {max_number}?")
             continue
 
         user_guess = int(user_input)
         attempts += 1
 
         if user_guess < random_number:
-            print("Ваше число меньше загаданного, попробуйте еще разок")
+            print("Your number is less than the one I guessed, try again")
         elif user_guess > random_number:
-            print("Ваше число больше загаданного, попробуйте еще разок")
+            print("Your number is greater than the guessed one, try again")
         else:
-            print(f"Вы угадали, поздравляем! Количество попыток: {attempts}")
+            print(f"You guessed it, congratulations! Number of attempts: {attempts}")
             break
 
-    play_again = input("Хотите сыграть еще раз? (y/n): ")
+    play_again = input("Do you want to play again? (y/n): ")
     if play_again.lower() != "y":
         break
 
-print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
+print("Thank you for playing the number guessing game. See you again...")
